a71741e755145feee8d273b239543bcf629ddb21
Triniti-11 <dalves11@icloud.com>
2021-09-02 21:55:33 -0400

    added something

---
+++ js/countdown.js
@@ -0,0 +1,26 @@
+console.log("did it work?")
+
+var countDownDate = new Date("Nov 30, 2021 18:00:00").getTime();
+
+var x = setInterval(function() {
+
+var  now = new Date().getTime();
+
+var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000); 
+
+
